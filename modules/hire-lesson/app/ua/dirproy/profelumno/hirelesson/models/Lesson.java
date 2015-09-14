e620cf2fa49fe1d5026ddc0858db20b63741b739
Paca <pacafuentes@hotmail.com>
2015-09-14 00:14:30 -0300

    Hire lesson module created, Lesson class created

--- modules/hire-lesson/app/ua/dirproy/profelumno/hirelesson/models/Lesson.java
+++ modules/hire-lesson/app/ua/dirproy/profelumno/hirelesson/models/Lesson.java
@@ -1,9 +1,12 @@
 package ua.dirproy.profelumno.hirelesson.models;
 
 import com.avaje.ebean.Model;
+import com.avaje.ebean.config.JsonConfig;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.sql.Time;
+import java.util.Date;
 
 /**
  * Created by Paca on 9/12/15.
@@ -12,4 +15,59 @@ import javax.persistence.Id;
 public class Lesson extends Model {
     @Id private Long id;
 
+    private Date dateTime;
+    private Time time;
+    //private Teacher teacher;
+    //private Student student;
+    private String place;
+    private String description;
+    private Float price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Float getPrice() {
+        return price;
+    }
+
+    public void setPrice(Float price) {
+        this.price = price;
+    }
 }
