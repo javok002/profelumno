33d96894752ac764774099923805be2809faa561
Francisco Di Giandomenico <francisco.di@ing.austral.edu.ar>
2015-09-28 18:32:17 -0300

    added routes and render and controller

--- modules/contact-form/app/ua/dirproy/profelumno/contactform/controllers/ContactForm.java
+++ modules/contact-form/app/ua/dirproy/profelumno/contactform/controllers/ContactForm.java
@@ -1,7 +1,16 @@
 package ua.dirproy.profelumno.contactform.controllers;
 
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import ua.dirproy.profelumno.contactform.view.html.*;
+
 /**
  * Created by yankee on 28/09/15.
  */
-public class ContactForm {
+public class ContactForm extends Controller {
+
+    public static Result contactFormView(){
+        return ok(contactform.render());
+    }
 }
