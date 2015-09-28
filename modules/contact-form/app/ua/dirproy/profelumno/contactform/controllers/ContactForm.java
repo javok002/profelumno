b201032b0028a7b3dfebf47b56f01c7a00b92d13
Francisco Di Giandomenico <francisco.di@ing.austral.edu.ar>
2015-09-28 18:43:17 -0300

    added post to routes, added method to ContactForm

--- modules/contact-form/app/ua/dirproy/profelumno/contactform/controllers/ContactForm.java
+++ modules/contact-form/app/ua/dirproy/profelumno/contactform/controllers/ContactForm.java
@@ -4,13 +4,22 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 import ua.dirproy.profelumno.contactform.view.html.*;
+import ua.dirproy.profelumno.mailsender.models.MailSenderUtil;
 
 /**
  * Created by yankee on 28/09/15.
  */
 public class ContactForm extends Controller {
 
-    public static Result contactFormView(){
+    public static Result contactFormView() {
         return ok(contactform.render());
+    }
+
+    public static void sendForm(String name, String email, String subject, String message) {
+        try {
+            MailSenderUtil.send(new String[]{"emailToSendTo@email.com"}, subject, message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
