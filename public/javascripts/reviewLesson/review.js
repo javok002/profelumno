75743762990b1b3bfc633e7bbe2fa73c31288b89
GustavoCarvallo <gustavo.carvallo@ing.austral.edu.ar>
2015-10-03 13:26:08 -0300

    View of lesson-review module

--- public/javascripts/reviewLesson/review.js
+++ public/javascripts/reviewLesson/review.js
@@ -5,51 +5,79 @@
 (function(){
     var app = angular.module('lessonReview', []);
 
-    app.controller('LessonReviewController', function($scope) {
-        this.reviewedLessons = reviewedLessons;
-        this.nonReviewedLessons = nonReviewedLessons;
+    app.controller('LessonReviewsController', function($scope) {
+        this.reviewedLessons = reviews.reviewed;
+        this.nonReviewedLessons = reviews.nonReviewed;
 
+
+        /* Useful functions */
+        //Used to know which review the user want to comment/review.
+        $scope.setIndex = function(index){
+            currentIndex = index;
+        }
     });
 
     app.controller('MakeReviewController', function($scope){
-        $scope.revEmail = "";
-        $scope.revStars = 3; //DEFAULT VALUE IT MUST BE CHANGE!!!
-        $scope.revComment = "";
+        this.nonReviewedLessons = reviews.nonReviewed;
+        this.currentStars ='';
+        $scope.review = {
+            email: this.nonReviewedLessons[currentIndex].user,
+            stars: 3, //Default value, It must be change!!!
+            comment: ""
+        };
 
         $scope.postReview = function(){
-            //Posteamos la data
 
+            /*
+            $http.post("/URL" + "Params").success(function(response){
+               //Do something with th response!!!
+            });
+            */
 
         };
+
+        /* Useful functions */
+
+        $scope.setStars = function(stars){
+            $scope.review.stars = stars;
+        }
     });
 
-    app.controller('LessonReviewNotificationController', function($scope) {
-        this.reviewedLessons = reviewedLessons;
-        this.nonReviewedLessons = nonReviewedLessons;
+    var countReviewed = 0;
+    var countNonReviewed = 0;
 
-    });
+    var reviews = {
+        reviewed: [
+            {   user: 'Here will appear the user name',
+                date: "12/2/15",
+                stars: 4,
+                comment: 'Here will appear the comment',
+                index: countReviewed++
+            },
+            {   user: 'Here will appear the user name',
+                date: "12/2/15",
+                stars: 4,
+                comment: 'Here will appear the comment',
+                index: countReviewed++
+            }
+        ],
+        nonReviewed: [
+            {   user: 'user1@mail',
+                date: "12/2/15",
+                stars: '',
+                comment: '',
+                index: countNonReviewed ++
+            },
+            {   user: 'user2@mail',
+                date: "12/2/15",
+                stars: '',
+                comment: '',
+                index: countNonReviewed++
+            }
+        ]
+    };
 
-    var reviewedLessons = [
-        {   user: 'Here will appear the user name',
-            date: "12/2/15",
-            stars: 4,
-            comment: 'Here will appear the comment'},
+    var currentIndex = 0;
 
-        {   user: 'Here will appear the user name',
-            date: "15/2/15",
-            stars: 2,
-            comment: 'Here will appear the comment'}
-    ];
-    var nonReviewedLessons = [
-        {   user: 'Here will appear the user name',
-            date: "12/2/15",
-            stars: "",
-            comment: ""},
-
-        {   user: 'Here will appear the user name',
-            date: "15/2/15",
-            stars: "",
-            comment: ""}
-    ];
 
 })();
