135f7a1ead532640aec1483877094d6375af02b0
Nicoo695 <nmburroni@gmail.com>
2015-10-03 18:33:23 -0300

    Encapsulado re lindo el isStudent y Teacher en Common

--- modules/common/app/ua/dirproy/profelumno/common/controllers/Common.java
+++ modules/common/app/ua/dirproy/profelumno/common/controllers/Common.java
@@ -1,11 +1,15 @@
 package ua.dirproy.profelumno.common.controllers;
 
+import com.avaje.ebean.Ebean;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 import play.twirl.api.Html;
+import ua.dirproy.profelumno.common.models.Student;
+import ua.dirproy.profelumno.common.models.Teacher;
 import ua.dirproy.profelumno.common.views.html.sidebar;
 import ua.dirproy.profelumno.common.views.html.topbar;
+import ua.dirproy.profelumno.user.models.User;
 
 /**
  * Created by Nicolás Burroni
@@ -19,6 +23,17 @@ public class Common extends Controller {
 
     public static Result topbar(){
         return ok(topbar.render());
+    }
+
+    /*final long userId = Long.parseLong(session("id"));
+    User user = Ebean.find(User.class, userId);*/
+
+    private static boolean isTeacher(User user) {
+        return Teacher.finder.where().eq("user", user).findUnique() != null;
+    }
+
+    private static boolean isStudent(User user) {
+        return Student.finder.where().eq("user", user).findUnique() != null;
     }
 
 }
