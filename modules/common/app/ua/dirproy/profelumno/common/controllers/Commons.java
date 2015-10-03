bd73b1249b3ab6bc0b2616459b0c9ae27069071c
Nicoo695 <nmburroni@gmail.com>
2015-10-03 17:50:00 -0300

    Chau architecture. Movidos top y side bar a common.

---
+++ modules/common/app/ua/dirproy/profelumno/common/controllers/Commons.java
@@ -0,0 +1,28 @@
+package ua.dirproy.profelumno.common.controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import play.twirl.api.Html;
+import ua.dirproy.profelumno.common.views.html.sidebar;
+import ua.dirproy.profelumno.common.views.html.topbar;
+
+/**
+ * Created by Nicolás Burroni
+ * Date: 28/09/15
+ */
+public class Commons extends Controller {
+
+    public static Result sidebar(){
+        return ok(sidebar.render());
+    }
+
+    public static Result topbar(){
+        return ok(topbar.render());
+    }
+
+    public static Html testContent() {
+        return new Html("<h1>Test Content Works!!!</h1>");
+    }
+
+}
