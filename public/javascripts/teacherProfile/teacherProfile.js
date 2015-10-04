cc36c34d9a40fedf003a36de410c74d046523078
Nicoo695 <nmburroni@gmail.com>
2015-10-04 12:54:56 -0300

    Ranking en el teacher dashboard

--- public/javascripts/teacherProfile/teacherProfile.js
+++ public/javascripts/teacherProfile/teacherProfile.js
@@ -4,12 +4,17 @@
 angular.module('app', [])
     .controller('DashboardController', ['$scope', '$http', function($scope, $http) {
 
-        $scope.ranking = 0;
-
         $scope.init = function() {
             $http.get('/teacher-profile/ranking')
                 .success(function(data) {
                     $scope.ranking = data;
+                })
+                .error(function(data) {
+                //    TODO warning message
+                });
+            $http.get('/teacher-profile/top-subjects')
+                .success(function(data) {
+                    $scope.topSubjects = data;
                 })
                 .error(function(data) {
                 //    TODO warning message
