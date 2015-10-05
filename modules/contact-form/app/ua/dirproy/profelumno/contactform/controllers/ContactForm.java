6676616b01c78a4c49078e5c7edc60914f6b0790
Francisco Di Giandomenico <francisco.di@ing.austral.edu.ar>
2015-10-05 16:46:58 -0300

    made the email sent even prettier

--- modules/contact-form/app/ua/dirproy/profelumno/contactform/controllers/ContactForm.java
+++ modules/contact-form/app/ua/dirproy/profelumno/contactform/controllers/ContactForm.java
@@ -33,7 +33,7 @@ public class ContactForm extends Controller {
         result.put("subject", subject);
         result.put("message", message);
 
-        String messageToSend = "New message from " + name + " at " + email + "\n\n" + message;
+        String messageToSend = "New message from: " + name + "\nAt: " + email + "\n\n" + message;
         try {
             MailSenderUtil.send(new String[]{"francisco.di@ing.austral.edu.ar"}, subject, messageToSend);
         } catch (Exception e) {
