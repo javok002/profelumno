baf3dc31ab5e103f22f98a5f8467aec520034eba
Paca <pacafuentes@hotmail.com>
2015-10-04 23:42:16 -0300

    studentprofile show teacher an subjects

--- public/javascripts/studentProfile/studentProfile.js
+++ public/javascripts/studentProfile/studentProfile.js
@@ -1,46 +1,55 @@
 angular.module('studentProfile', [])
 
     .controller('StudentCtrl',['$scope', '$http', function($scope, $http) {
-        $scope.data = '';
         $scope.classes = ['danger', 'info', 'warning', 'success'];
-
-        $http.get('/student-profile/get-info', config).then(successCallback);
-
-        succesCallback = function(response) {
-            $scope.data = response.data;
-        };
+        $http.get('/student-profile/get-info').then(function(data) {
+            $scope.data = data.data;
+        });
     }])
 
     .directive('subjects', function() {
         return {
             restrict: 'E',
-            scope : {
-                data : '=data'
-            },
-            link : function (scope){
-               var i = 1;
-            },
             template:
-                <!-- Main content -->
-            '<section class="content">' +
-                '<div class="row">' +
-                    '<div class="col-md-4">' +
-                        '<div class="box box-default">' +
-                            '<div class="box-header with-border">' +
-                                '<i class="fa fa-book"> </i>' +
-                                '<h3 class="box-title"> Materias</h3>' +
-                            '</div> <!-- /.box-header -->' +
-                            '<div class="box-body">' +
-                                '<div ng-repeat="subject in data.subjects">' +
-                                    '<div class="callout callout-{{classes[$index % 4]}}">' +
-                                        '<h4>{{ subject.text }}</h4>' +
-                                    '</div>' +
-                                '</div>' +
-                            '</div> <!-- /.box-body -->' +
+            '<div class="col-md-6">' +
+                '<div class="box box-default">' +
+                    '<div class="box-header with-border">' +
+                        '<i class="fa fa-book"> </i>' +
+                        '<h3 class="box-title"> Subjects</h3>' +
+                    '</div> <!-- /.box-header -->' +
+                    '<div class="box-body">' +
+                        '<div ng-repeat="subject in data.Subjects">' +
+                            '<div class="callout callout-{{classes[$index % 4]}}">' +
+                                '<h4>{{ subject.text }}</h4>' +
+                            '</div>' +
                         '</div>' +
+                    '</div> <!-- /.box-body -->' +
+                    '<div class="box-footer">' +
+                        '<button class="btn-default"><a href="/modify-student/edit">Edit my subjects</a></button>' +
                     '</div>' +
-                '</div> <!-- /.row -->' +
-            '</section> <!-- /.content -->'
+                '</div>' +
+            '</div>'
+        }
+    })
 
+    .directive('teachers', function() {
+        return {
+            restrict: 'E',
+            template:
+            '<div class="col-md-6">' +
+                '<div class="box box-default">' +
+                    '<div class="box-header with-border">' +
+                        '<i class="fa fa-user"> </i>' +
+                        '<h3 class="box-title"> Teachers</h3>' +
+                    '</div> <!-- /.box-header -->' +
+                    '<div class="box-body">' +
+                        '<div ng-repeat="teacher in data.Teachers">' +
+                            '<div class="callout callout-{{classes[$index % 4]}}">' +
+                                '<h4>{{ teacher }}</h4>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div> <!-- /.box-body -->' +
+                '</div>' +
+            '</div>'
         }
     });
