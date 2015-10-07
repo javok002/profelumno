876fc12d8258439c549095d47778e9fc7ce8377c
Nicolás Rudolph <nicolas.rudolph2@gmail.com>
2015-10-06 22:38:05 -0300

    teacher-modification modify

--- public/javascripts/teacherModify/teacherModify.js
+++ public/javascripts/teacherModify/teacherModify.js
@@ -65,9 +65,72 @@ app.controller('TeacherInfoController', ['$scope', '$http', 'fileUpload', functi
         return !$scope.errors.incomplete && !$scope.errors.invalid && !$scope.errors.teacherAge;
     };
 
+    $scope.back=function(){
+        window.location.href = "/teacher-profile"
+    };
     $scope.verifyDesc= function(){
         $scope.errors.length=edit.u.description.length<50;
     };
+
+    //MAPS
+    $scope.gotoCurrentLocation = function () {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                var c = position.coords;
+                $scope.gotoLocation(c.latitude, c.longitude);
+            });
+            return true;
+        }
+        return false;
+    };
+
+    $scope.gotoLocation = function (lat, lon) {
+        if ($scope.lat != lat || $scope.lon != lon) {
+            $scope.loc = { lat: lat, lon: lon };
+            $scope.marker={lat:$scope.loc.lat,lon:$scope.loc.lon};
+            $scope.$apply("marker");
+            if (!$scope.$$phase) $scope.$apply("loc");
+        }
+    };
+    $scope.geoCode = function () {
+        if ($scope.search && $scope.search.length > 0) {
+            if (!this.geocoder) this.geocoder = new google.maps.Geocoder();
+            this.geocoder.geocode({ 'address': $scope.search }, function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    var loc = results[0].geometry.location;
+                    arrAddress = results[0].address_components;
+                    $scope.search = results[0].formatted_address;
+                    $scope.gotoLocation(loc.lat(), loc.lng());
+                    return loc;
+                } else {
+                    alert("Sorry, this search produced no results.");
+                }
+            });
+        }
+    };
+    $scope.loc =$scope.geoCode();
+
+    $scope.getCity = function () {
+        for(var i=0;i<arrAddress.length;i++)
+        {
+            if (arrAddress[i].types[0] == "locality") {
+                return arrAddress[i].long_name;
+            }
+        }
+        return null;
+    };
+
+    $scope.getNeighbourhood = function () {
+        for(var i=0;i<arrAddress.length;i++)
+        {
+            if (arrAddress[i].types.length>=2 && arrAddress[i].types[1] == "sublocality") {
+                return arrAddress[i].long_name;
+            }
+        }
+        return null;
+    };
+
+    //SUBMIT
     $scope.submit = function () {
         if (!verify())
             return;
@@ -80,7 +143,7 @@ app.controller('TeacherInfoController', ['$scope', '$http', 'fileUpload', functi
         $http.post('modify-teacher/teacher-modification-post', edit.u)
             .success(function (data) {
                 $scope.errors = {incomplete: false, invalid: false, teacherAge: false, taken: false, length: false};
-                window.location.href = data;
+                window.location.href = "/teacher-profile"
                 /*alert(JSON.stringify(data));*/
             })
             .error(function (data) {
@@ -155,11 +218,139 @@ app.service('fileUpload', ['$http', function ($http) {
             headers: {'Content-Type': undefined}
         })
             .success(function (data, status, headers, config) {
-                edit.u.user.profilePicture = data;
-                scope.imageUrl=data;
+                window.location.href = "/teacher-profile"
+                /*edit.u.user.profilePicture = data;
+                scope.imageUrl=data;*/
                 //window.location.href=data;
             })
             .error(function () {
             });
     }
 }]);
+
+app.directive("appMap", function () {
+    return {
+        restrict: "E",
+        replace: true,
+        template: "<div></div>",
+        scope: {
+            center: "=",        // Center point on the map (e.g. <code>{ latitude: 10, longitude: 10 }</code>).
+            markers: "=",       // Array of map markers (e.g. <code>[{ lat: 10, lon: 10, name: "hello" }]</code>).
+            width: "@",         // Map width in pixels.
+            height: "@",        // Map height in pixels.
+            zoom: "@",          // Zoom level (one is totally zoomed out, 25 is very much zoomed in).
+            mapTypeId: "@",     // Type of tile to show on the map (roadmap, satellite, hybrid, terrain).
+            panControl: "@",    // Whether to show a pan control on the map.
+            zoomControl: "@",   // Whether to show a zoom control on the map.
+            scaleControl: "@"   // Whether to show scale control on the map.
+        },
+        link: function (scope, element, attrs) {
+            var toResize, toCenter;
+            var map;
+            var currentMarkers;
+
+            // listen to changes in scope variables and update the control
+            var arr = ["width", "height", "markers", "mapTypeId", "panControl", "zoomControl", "scaleControl"];
+            for (var i = 0, cnt = arr.length; i < arr.length; i++) {
+                scope.$watch(arr[i], function () {
+                    cnt--;
+                    if (cnt <= 0) {
+                        updateControl();
+                    }
+                });
+            }
+
+            // update zoom and center without re-creating the map
+            scope.$watch("zoom", function () {
+                if (map && scope.zoom)
+                    map.setZoom(scope.zoom * 1);
+            });
+            scope.$watch("center", function () {
+                if (map && scope.center)
+                    map.setCenter(getLocation(scope.center));
+            });
+
+            // update the control
+            function updateControl() {
+
+                // update size
+                if (scope.width) element.width(scope.width);
+                if (scope.height) element.height(scope.height);
+
+                // get map options
+                var options =
+                {
+                    center: new google.maps.LatLng(40, -73),
+                    zoom: 6,
+                    mapTypeId: "roadmap"
+                };
+                if (scope.center) options.center = getLocation(scope.center);
+                if (scope.zoom) options.zoom = scope.zoom * 1;
+                if (scope.mapTypeId) options.mapTypeId = scope.mapTypeId;
+                if (scope.panControl) options.panControl = scope.panControl;
+                if (scope.zoomControl) options.zoomControl = scope.zoomControl;
+                if (scope.scaleControl) options.scaleControl = scope.scaleControl;
+
+                // create the map
+                map = new google.maps.Map(element[0], options);
+
+                // update markers
+                updateMarkers();
+
+                // listen to changes in the center property and update the scope
+                google.maps.event.addListener(map, 'center_changed', function () {
+
+                    // do not update while the user pans or zooms
+                    if (toCenter) clearTimeout(toCenter);
+                    toCenter = setTimeout(function () {
+                        if (scope.center) {
+
+                            // check if the center has really changed
+                            if (map.center.lat() != scope.center.lat ||
+                                map.center.lng() != scope.center.lon) {
+
+                                // update the scope and apply the change
+                                scope.center = { lat: map.center.lat(), lon: map.center.lng() };
+                                if (!scope.$$phase) scope.$apply("center");
+                            }
+                        }
+                    }, 500);
+                });
+            }
+
+            // update map markers to match scope marker collection
+            function updateMarkers() {
+                if (map && scope.markers) {
+
+                    // clear old markers
+                    if (currentMarkers != null) {
+                        for (var i = 0; i < currentMarkers.length; i++) {
+                            //currentMarkers[i] = m.setMap(null);
+                        }
+                    }
+
+                    // create new markers
+                    currentMarkers = [];
+                    var markers = scope.markers;
+                    if (angular.isString(markers)) markers = scope.$eval(scope.markers);
+                    for (var i = 0; i < markers.length; i++) {
+                        var m = markers[i];
+                        if(m!=null) {
+                            var loc = new google.maps.LatLng(m.lat, m.lon);
+                            var mm = new google.maps.Marker({position: loc, map: map, title: m.name});
+                            currentMarkers.push(mm);
+                        }
+                    }
+                }
+            }
+
+            // convert current location to Google maps location
+            function getLocation(loc) {
+                if (loc == null) return new google.maps.LatLng(40, -73);
+                if (angular.isString(loc)) loc = scope.$eval(loc);
+                return new google.maps.LatLng(loc.lat, loc.lon);
+            }
+        }
+    };
+});
+
