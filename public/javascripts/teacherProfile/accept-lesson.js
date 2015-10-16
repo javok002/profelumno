b8401a86bf9a1451567fa99da81830085da2b574
FacuBertolosso <facundo.bertolosso@ing.austral.edu.ar>
2015-10-16 12:06:56 -0300

    accepting lesson changes

--- public/javascripts/teacherProfile/accept-lesson.js
+++ public/javascripts/teacherProfile/accept-lesson.js
@@ -2,15 +2,20 @@
  * Created by facundo on 1/10/15.
  */
 var app = angular.module('acceptLesson', []);
-app.controller('AcceptController', ['$scope', '$http', function ($scope, $http) {
+function loadLessons($http, $scope) {
     $http.get("/teacher-profile/get-lessons")
         .success(function (data) {
             $scope.teacherLessons = data;
         });
-    $scope.decision = function (lessonId, answer) {
+}
+app.controller('AcceptController', ['$scope', '$http', function ($scope, $http) {
+    loadLessons($http, $scope);
+    $scope.decision = function (lessonId, answer, button) {
+        button.currentTarget.innerHTML = '<span><i class="fa fa-spinner fa-pulse"></i></span>';
+        button.currentTarget.disabled = true;
         $http.get("/teacher-profile/decision?answer=" + answer+"&stringLessonId="+lessonId)
             .success(function (){
-                document.getElementById(lessonId).style.display = "none";
+                loadLessons($http, $scope);
                 document.getElementById('showModal').click();
             })
             .error(function (){
