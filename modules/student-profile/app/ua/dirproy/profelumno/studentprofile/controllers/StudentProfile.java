1f333d130275d002359f019c0a5dff8491c7906b
Juan Ignacio Longo <juan.longo@ing.austral.edu.ar>
2015-10-17 17:21:44 -0300

    correciones studentprofile back

--- modules/student-profile/app/ua/dirproy/profelumno/studentprofile/controllers/StudentProfile.java
+++ modules/student-profile/app/ua/dirproy/profelumno/studentprofile/controllers/StudentProfile.java
@@ -38,8 +38,18 @@ public class StudentProfile extends Controller {
 
         //ultimos profesores con los que tuve contacto
         final HashSet<Teacher> lastTeachers = new HashSet<>();
+        final ArrayList<Subject> teacherSubject = new ArrayList<>();
         for (Lesson lesson : lastLessonsStudent) {
             lastTeachers.add(lesson.getTeacher());
+            teacherSubject.add(lesson.getSubject());
+        }
+
+        //lessons que no estan con rating
+        final ArrayList<Lesson> lessonsWithNoReview = new ArrayList<>();
+        for (Lesson lesson : lastLessonsStudent) {
+            if (lesson.getStudentReview() == null){
+                lessonsWithNoReview.add(lesson);
+            }
         }
 
         //map que voy a devolver
@@ -47,6 +57,8 @@ public class StudentProfile extends Controller {
         answer.put("Subjects", subjects);
         answer.put("Lessons", lastLessonsStudent);
         answer.put("Teachers", lastTeachers);
+        answer.put("Teacher's subject", teacherSubject);
+        answer.put("Lessons with no review", lessonsWithNoReview);
         return ok(Json.toJson(answer));
     }
 }
