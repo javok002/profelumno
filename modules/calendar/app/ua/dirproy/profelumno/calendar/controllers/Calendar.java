01e3f222326267056e5a5f98a11285620afcc46e
Nicolas Burroni <nmburroni@gmail.com>
2015-10-26 18:48:37 -0300

    Creado Calendar sin información.

--- modules/calendar/app/ua/dirproy/profelumno/calendar/controllers/Calendar.java
+++ modules/calendar/app/ua/dirproy/profelumno/calendar/controllers/Calendar.java
@@ -2,15 +2,23 @@ package ua.dirproy.profelumno.calendar.controllers;
 
 import authenticate.Authenticate;
 
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
 import ua.dirproy.profelumno.common.models.Student;
 import ua.dirproy.profelumno.common.models.Teacher;
 
+import ua.dirproy.profelumno.calendar.views.html.calendar;
+import ua.dirproy.profelumno.user.models.User;
 
 @Authenticate({Teacher.class, Student.class})
 public class Calendar extends Controller {
 
-    
+    public static Result calendarView() { return ok(calendar.render()); }
+
+    public static Result getUserName() {
+        User user = User.finder.byId(Long.parseLong(session("id")));
+        return user != null ? ok(user.getName() + " " + user.getSurname()) : badRequest();
+    }
 
 }
