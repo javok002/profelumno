6ca6966ce91baf5c4e60bb59ef5a50a526d02a31
federuiz <ruizfederico95@gmail.com>
2015-11-02 18:37:28 -0300

    change test to false

--- modules/teacher-subscription/app/ua/dirproy/profelumno/subscription/controllers/TeacherSubscription.java
+++ modules/teacher-subscription/app/ua/dirproy/profelumno/subscription/controllers/TeacherSubscription.java
@@ -27,7 +27,7 @@ import java.util.Date;
 @Authenticate({Teacher.class})
 public class TeacherSubscription extends Controller{
 
-    static boolean test = true;
+    static boolean test = false;
 
     public static Result validateForm(){
         final long userId= Long.parseLong(session("id"));
