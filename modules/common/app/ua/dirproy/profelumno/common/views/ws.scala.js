e7dc8dc7bd31c9577a0d5e439cb0deb31b65ba6e
FranciscoCeriani <francisco.a.ceriani@gmail.com>
2015-11-06 20:09:48 -0300

    eeeeh macaareena

--- modules/common/app/ua/dirproy/profelumno/common/views/ws.scala.js
+++ modules/common/app/ua/dirproy/profelumno/common/views/ws.scala.js
@@ -1,6 +1,174 @@
-$(function() {
-    angular.bootstrap(document.getElementById("chat-bar"),['app']);
-});
+//this variable represents the total number of popups can be displayed according to the viewport width
+var total_popups = 0;
+
+//arrays of popups ids
+var popups = [];
+
+//this function can remove a array element.
+Array.remove = function(array, from, to) {
+    var rest = array.slice((to || from) + 1 || array.length);
+    array.length = from < 0 ? array.length + from : from;
+    return array.push.apply(array, rest);
+};
+
+
+//this is used to close a popup
+function close_popup(id)
+{
+    for(var iii = 0; iii < popups.length; iii++)
+    {
+        if(id == popups[iii])
+        {
+            Array.remove(popups, iii);
+
+            document.getElementById(id).style.display = "none";
+
+            calculate_popups();
+
+            return;
+        }
+    }
+}
+
+//displays the popups. Displays based on the maximum number of popups that can be displayed on the current viewport width
+function display_popups()
+{
+    var right = 220;
+
+    var iii = 0;
+    for(iii; iii < total_popups; iii++)
+    {
+        if(popups[iii] != undefined)
+        {
+            var element = document.getElementById(popups[iii]);
+            element.style.right = right + "px";
+            right = right + 320;
+            element.style.display = "block";
+        }
+    }
+
+    for(var jjj = iii; jjj < popups.length; jjj++)
+    {
+        var element = document.getElementById(popups[jjj]);
+        element.style.display = "none";
+    }
+}
+
+//creates markup for a new popup. Adds the id to popups array.
+function register_popup(id, name)
+{
+
+    for(var iii = 0; iii < popups.length; iii++)
+    {
+        //already registered. Bring it to front.
+        if(id == popups[iii])
+        {
+            Array.remove(popups, iii);
+
+            popups.unshift(id);
+
+            calculate_popups();
+
+
+            return;
+        }
+    }
+    var element2= '<div class="box box-danger direct-chat direct-chat-danger">\
+        <div class="box-header with-border">\
+    <h3 class="box-title">Direct Chat</h3>\
+    <div class="box-tools pull-right">\
+    <span data-toggle="tooltip" title="3 New Messages" class="badge bg-red">3</span>\
+<button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>\
+    <button class="btn btn-box-tool"><a href="javascript:close_popup(\''+ id +'\');">&#10005;</a></button>\
+    </div>\
+    </div>\
+<div class="box-body">\
+    <div class="direct-chat-messages">\
+        <div class="direct-chat-msg">\
+            <div class="direct-chat-info clearfix">\
+                <span class="direct-chat-name pull-left">Alexander Pierce</span>\
+                <span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>\
+            </div>\
+            <img class="direct-chat-img" src="../dist/img/user1-128x128.jpg" alt="message user image">\
+                <div class="direct-chat-text">\
+                    Is this template really for free? Thats unbelievable!\
+                </div>\
+            </div>\
+            <div class="direct-chat-msg right">\
+                <div class="direct-chat-info clearfix">\
+                    <span class="direct-chat-name pull-right">Sarah Bullock</span>\
+                    <span class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>\
+                </div>\
+                <img class="direct-chat-img" src="../dist/img/user3-128x128.jpg" alt="message user image">\
+                    <div class="direct-chat-text">\
+                        You better believe it!\
+                    </div>\
+                </div>\
+            </div>\
+            <div class="direct-chat-contacts">\
+                <ul class="contacts-list">\
+                    <li>\
+                        <a href="#">\
+                            <img class="contacts-list-img" src="../dist/img/user1-128x128.jpg" alt="Contact Avatar">\
+                                <div class="contacts-list-info">\
+              <span class="contacts-list-name">\
+                Count Dracula\
+                <small class="contacts-list-date pull-right">2/28/2015</small>\
+              </span>\
+                                    <span class="contacts-list-msg">How have you been? I was...</span>\
+                                </div>\
+                            </a>\
+                        </li>\
+                    </ul>\
+                </div>\
+            </div>\
+            <div class="box-footer">\
+                <div class="input-group">\
+                    <input type="text" name="message" placeholder="Type Message ..." class="form-control">\
+      <span class="input-group-btn">\
+        <button type="button" class="btn btn-danger btn-flat">Send</button>\
+      </span>\
+                    </div>\
+                </div>\
+            </div>';
+
+
+
+    var element = '<div class="popup-box chat-popup" id="'+ id +'">';
+    element+=element2;
+
+    document.getElementsByTagName("body")[0].innerHTML = document.getElementsByTagName("body")[0].innerHTML + element;
+
+    popups.unshift(id);
+
+    calculate_popups();
+
+}
+
+//calculate the total number of popups suitable and then populate the toatal_popups variable.
+function calculate_popups()
+{
+    //var width = window.innerWidth;
+    //if(width < 540)
+    //{
+    //    total_popups = 0;
+    //}
+    //else
+    //{
+    //    width = width - 200;
+    //    //320 is width of a single popup box
+    //    total_popups = parseInt(width/320);
+    //}
+    total_popups=2;
+    display_popups();
+
+}
+
+//recalculate when window is loaded and also when window is resized.
+window.addEventListener("resize", calculate_popups);
+window.addEventListener("load", calculate_popups);
+
+
 angular.module('app', [])
     .controller('ChatController', ['$scope', '$http', function ($scope, $http) {
         var userInSession;
@@ -103,171 +271,4 @@ angular.module('app', [])
 
 
 
-//this function can remove a array element.
-Array.remove = function(array, from, to) {
-    var rest = array.slice((to || from) + 1 || array.length);
-    array.length = from < 0 ? array.length + from : from;
-    return array.push.apply(array, rest);
-};
 
-//this variable represents the total number of popups can be displayed according to the viewport width
-var total_popups = 0;
-
-//arrays of popups ids
-var popups = [];
-
-//this is used to close a popup
-function close_popup(id)
-{
-    for(var iii = 0; iii < popups.length; iii++)
-    {
-        if(id == popups[iii])
-        {
-            Array.remove(popups, iii);
-
-            document.getElementById(id).style.display = "none";
-
-            calculate_popups();
-
-            return;
-        }
-    }
-}
-
-//displays the popups. Displays based on the maximum number of popups that can be displayed on the current viewport width
-function display_popups()
-{
-    var right = 220;
-
-    var iii = 0;
-    for(iii; iii < total_popups; iii++)
-    {
-        if(popups[iii] != undefined)
-        {
-            var element = document.getElementById(popups[iii]);
-            element.style.right = right + "px";
-            right = right + 320;
-            element.style.display = "block";
-        }
-    }
-
-    for(var jjj = iii; jjj < popups.length; jjj++)
-    {
-        var element = document.getElementById(popups[jjj]);
-        element.style.display = "none";
-    }
-}
-
-//creates markup for a new popup. Adds the id to popups array.
-function register_popup(id, name)
-{
-
-    for(var iii = 0; iii < popups.length; iii++)
-    {
-        //already registered. Bring it to front.
-        if(id == popups[iii])
-        {
-            Array.remove(popups, iii);
-
-            popups.unshift(id);
-
-            calculate_popups();
-
-
-            return;
-        }
-    }
-    var element2= '<div class="box box-danger direct-chat direct-chat-danger" ng-controller="ChatController as controller">\
-        <div class="box-header with-border">\
-    <h3 class="box-title">name</h3>\
-    <div class="box-tools pull-right">\
-    <span data-toggle="tooltip" title="3 New Messages" class="badge bg-red">3</span>\
-<button class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="Collapse"><i class="fa fa-minus"></i></button>\
-    <button class="btn btn-box-tool"><a href="javascript:close_popup(\''+ id +'\');">&#10005;</a></button>\
-    </div>\
-    </div>\
-<div class="box-body">\
-    <div class="direct-chat-messages">\
-        <div class="direct-chat-msg">\
-            <div class="direct-chat-info clearfix">\
-                <span class="direct-chat-name pull-left">Alexander Pierce</span>\
-                <span class="direct-chat-timestamp pull-right">23 Jan 2:00 pm</span>\
-            </div>\
-            <img class="direct-chat-img" src="../dist/img/user1-128x128.jpg" alt="message user image">\
-                <div class="direct-chat-text">\
-                    Is this template really for free? Thats unbelievable!\
-                </div>\
-            </div>\
-            <div class="direct-chat-msg right">\
-                <div class="direct-chat-info clearfix">\
-                    <span class="direct-chat-name pull-right">Sarah Bullock</span>\
-                    <span class="direct-chat-timestamp pull-left">23 Jan 2:05 pm</span>\
-                </div>\
-                <img class="direct-chat-img" src="../dist/img/user3-128x128.jpg" alt="message user image">\
-                    <div class="direct-chat-text">\
-                        You better believe it!\
-                    </div>\
-                </div>\
-            </div>\
-            <div class="direct-chat-contacts">\
-                <ul class="contacts-list">\
-                    <li>\
-                        <a href="#">\
-                            <img class="contacts-list-img" src="../dist/img/user1-128x128.jpg" alt="Contact Avatar">\
-                                <div class="contacts-list-info">\
-              <span class="contacts-list-name">\
-                Count Dracula\
-                <small class="contacts-list-date pull-right">2/28/2015</small>\
-              </span>\
-                                    <span class="contacts-list-msg">How have you been? I was...</span>\
-                                </div>\
-                            </a>\
-                        </li>\
-                    </ul>\
-                </div>\
-            </div>\
-            <div class="box-footer">\
-                <div class="input-group">\
-                    <input type="text" name="message" placeholder="Type Message ..." class="form-control">\
-      <span class="input-group-btn">\
-        <button type="button" class="btn btn-danger btn-flat">Send</button>\
-      </span>\
-                    </div>\
-                </div>\
-            </div>';
-
-
-
-    var element = '<div class="popup-box chat-popup" id="'+ id +'">';
-    element+=element2;
-
-    document.getElementsByTagName("body")[0].innerHTML = document.getElementsByTagName("body")[0].innerHTML + element;
-
-    popups.unshift(id);
-
-    calculate_popups();
-
-}
-
-//calculate the total number of popups suitable and then populate the toatal_popups variable.
-function calculate_popups()
-{
-    var width = window.innerWidth;
-    if(width < 540)
-    {
-        total_popups = 0;
-    }
-    else
-    {
-        width = width - 200;
-        //320 is width of a single popup box
-        total_popups = parseInt(width/320);
-    }
-
-    display_popups();
-
-}
-
-//recalculate when window is loaded and also when window is resized.
-window.addEventListener("resize", calculate_popups);
-window.addEventListener("load", calculate_popups);
