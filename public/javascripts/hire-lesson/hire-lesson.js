b06a6b46a312e6d4fbc3b3bba66b7aaafc4e423b
Paca <pacafuentes@hotmail.com>
2015-11-08 22:16:11 -0300

    showing lessons alreafy accepted on hire lesson calendar

--- public/javascripts/hire-lesson/hire-lesson.js
+++ public/javascripts/hire-lesson/hire-lesson.js
@@ -5,8 +5,9 @@ angular.module('profLesson', [])
 
     .controller('HireCtrl',['$scope', '$http', function($scope, $http) {
         $scope.comment = '';
-        $scope.address= '';
         $scope.lessonDate= '';
+        $scope.selected= '';
+
         $scope.setSubjects = function (teacherId) {
             var subs = {};
             $http.get('/hire-lesson/get-subjects?teacherId='+teacherId)
@@ -16,13 +17,13 @@ angular.module('profLesson', [])
             return subs;
         };
         $scope.selected = {};
-        $scope.postLesson = function(teacherId) {
+        $scope.postLesson = function(teacherId, dateTime) {
             data = {
                 address:$scope.address,
                 comment:$scope.comment,
                 teacherId:teacherId,
                 subjectId:$scope.selected,
-                dateTime:$scope.dateTime
+                dateTime:dateTime
             };
             $http.post('/hire-lesson/new', data).then(successCallback, errorCallback);
             $("#loadingModal").modal("show");
@@ -40,8 +41,6 @@ angular.module('profLesson', [])
             $("#loadingModal").modal("hide");
         };
 
-        $('.datepicker').datepicker('startDate', Date.now());
-
     }])
 
     .directive('hireLesson',['$http', function($http){
@@ -54,6 +53,18 @@ angular.module('profLesson', [])
             },
             link : function(scope){
                 scope.date = 'date';
+
+                spanishDays = ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'];
+
+                toDateTimeString = function (date) {
+                    return [spanishDays[date.getDay()], date.getDate() + '/' + date.getMonth() + '/' + date.getFullYear(), 'a las', date.getHours() + ':' + date.getMinutes()].join(" ");
+                };
+
+                Date.prototype.addHours= function(h){
+                    this.setHours(this.getHours()+h);
+                    return this;
+                };
+
                 scope.init = function (calendarIndex) {
 
                     $http.get("/calendar/get-user-name")
@@ -64,9 +75,11 @@ angular.module('profLesson', [])
                     $http.get("/calendar/get-lessons")
                         .success(function (data) {
                             data.forEach(function(lesson) {
-                                var student = lesson.student.user;
+                                var teacher = lesson.teacher.user;
                                 lesson.title = lesson.subject.text /*+ ' a ' + student.name + ' ' + student.surname*/;
                                 lesson.date = new Date(lesson.dateTime);
+                                lesson.end = angular.copy(lesson.date).addHours(lesson.duration.seconds / 3600);
+                                lesson.lessonAcepted = true;
                             });
 
                             scope.data = data;
@@ -79,7 +92,8 @@ angular.module('profLesson', [])
                                     right: 'month,agendaWeek'
                                 },
                                 eventClick: function (event, jsEvent, view) {
-
+                                    scope.dateTime = new Date(event.dateTime);
+                                    if (!event.lessonAcepted) $('#datepicker' + scope.index).val(toDateTimeString(scope.dateTime));
                                 },
                                 events: scope.data
                             });
@@ -138,17 +152,15 @@ angular.module('profLesson', [])
                                     '</div>' +
 
                                     '<div class="input-group">' +
-                                        '<!--<input type="text" class="form-control datepicker" datepicker="user.birthday" placeholder="Fecha de Nacimiento" name="birthday" required ng-model="user.birthday">' +
-                                        '<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>--!>' +
                                         '<span class="input-group-addon"><i class="glyphicon glyphicon-calendar"></i></span>' +
-                                        '<input ng-model="dateTime" type="date" class="form-control" name="birthday" required ng-model="user.birthday" ng-class="{&#39;default-option&#39;: user.birthday == undefined}">' +
+                                        '<input style="width: 100%;height: 28px;" id="datepicker{{index}}" ng-model="dateTime" value="">' +
                                     '</div>'+
                                         '<div class="form-group">' +
                                             '<label>Comentario</label>' +
                                             '<textarea class="form-control" rows="3" placeholder="Dejar comentario ..." ng-model="comment"></textarea>' +
                                         '</div>' +
                                     '</div><!-- /.box-body -->' +
-                                    '<button ng-click="postLesson(teacherId)" class="btn btn-primary" data-dismiss="modal">Enviar</button>' +
+                                    '<button ng-click="postLesson(teacherId, dateTime)" class="btn btn-primary" data-dismiss="modal">Enviar</button>' +
                             '</div><!-- /.box -->' +
                         '</div>' +
                         '<div class="modal-footer">' +
