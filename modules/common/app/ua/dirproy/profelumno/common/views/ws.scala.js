4583b5444e923c7f75cae7fc4d09c273418513f4
GustavoCarvallo <gustavo.carvallo@ing.austral.edu.ar>
2015-11-11 12:59:31 -0300

    Chat final edition (unlimited)

--- modules/common/app/ua/dirproy/profelumno/common/views/ws.scala.js
+++ modules/common/app/ua/dirproy/profelumno/common/views/ws.scala.js
@@ -11,16 +11,47 @@ var total_popups = 0;
 //arrays of popups ids
 var popups = [];
 
+//this variable is to avoid display_open_chats execute more than once when a page is load.
+var timesChatControllerExecute = 0;
+
+//this variable try to solved the problem of angular execute sequence, only for the display_open_chats
+var currentUserChatIndex = 0;
+
 //this is used to close a popup
-function close_popup(id)
+function close_popup(chatData)
 {
+    //Code of Session Storage
+    var chatIdAndUserSessionId = chatData.split("-");
+    var chatID = chatIdAndUserSessionId[0];
+    var userInSessionId = chatIdAndUserSessionId[1];
+    var chatToId = chatIdAndUserSessionId[2];
+
+    var savedChat = JSON.parse(sessionStorage.getItem(userInSessionId));
+
+    var i = 0;
+    var indexToDelete = 0;
+    for(i; i < savedChat.length; i++){
+        if(savedChat[i].savedChatToId == chatToId){
+            indexToDelete = i;
+        }
+    }
+    savedChat.splice(indexToDelete,1);
+
+    sessionStorage.removeItem(userInSessionId);
+
+    if(savedChat.length > 0){
+        var chatToSave = JSON.stringify(savedChat);
+        sessionStorage.setItem(userInSessionId, chatToSave);
+    }
+    //End of session storage code
+
     for(var iii = 0; iii < popups.length; iii++)
     {
-        if(id == popups[iii])
+        if(chatID == popups[iii])
         {
             Array.remove(popups, iii);
 
-            document.getElementById(id).style.display = "none";
+            document.getElementById(chatID).style.display = "none";
 
             calculate_popups();
 
@@ -28,12 +59,108 @@ function close_popup(id)
         }
     }
 }
+//This function manage all the code for displaying the chats open when the user changes the windows.
+function display_open_chats(userIdSession){
+    
+    if(timesChatControllerExecute == 0){
+        var objLoaded = JSON.parse(sessionStorage.getItem(userIdSession));
+        if(objLoaded != null){
+            var i = 0;
+            for(i ; i < objLoaded.length; i++){
+                currentUserChatIndex = i;
+                var loadChatToId = objLoaded[i].savedChatToId;
+                var $http = angular.element($("#chatCtrl")).injector().get('$http');
+                $http.get('../common/getChat?userId='+loadChatToId)
+                    .success(function (data, status, headers, config) {
+                        var loadedChat = data.chat;
+                        loadChatToId = objLoaded[currentUserChatIndex].savedChatToId;
+                        var loadName = objLoaded[currentUserChatIndex].savedName;
+                        initialize_open_chats(loadedChat.id, userIdSession,loadChatToId,loadName);
+                        var messages = loadedChat.messages;
+                        var ii = 0;
+                        for(ii ; ii < messages.length; ii++){
+                            var message=messages[ii];
+                            if (message.author.id==userIdSession){
+                                angular.element('#socket-messages'+loadedChat.id).append('<div class="direct-chat-msg right">\
+                            <div class="direct-chat-info clearfix">\
+                                <span class="direct-chat-name pull-right">'+ message.author.name+" "+ message.author.surname+'</span>\
+                                <span class="direct-chat-timestamp pull-left">' + message.date + '</span>\
+                            </div>\
+                            <div class="direct-chat-text">\
+                                '+message.msg+'\
+                            </div></div>');
+                            }else{
+                                angular.element('#socket-messages'+loadedChat.id).append('<div class="direct-chat-msg">\
+                            <div class="direct-chat-info clearfix">\
+                                <span class="direct-chat-name pull-left">'+ message.author.name+" "+ message.author.surname+'</span>\
+                                <span class="direct-chat-timestamp pull-right">'+message.date+'</span>\
+                            </div>\
+                            <div class="direct-chat-text">\
+                                '+message.msg+'\
+                            </div></div>');
+                            }
+                            updateScroll();
+                        }
+                        currentUserChatIndex --;
+                    }).
+                    error(function () {
+                    });
+            }
+        }
+        timesChatControllerExecute ++;
+    }
+    else timesChatControllerExecute = 0;
+}
+//This function help the display_open_chats function.
+function initialize_open_chats(chatID, userInSessionID,chatToID, userName){
+    for(var iii = 0; iii < popups.length; iii++)
+    {
+        //already registered. Bring it to front.
+        if(chatID == popups[iii])
+        {
+            Array.remove(popups, iii);
+
+            popups.unshift(chatID);
+
+            calculate_popups();
+
+
+            return;
+        }
+    }
+    var chatIdAndUserInSessionId = chatID + "-" + userInSessionID + "-" + chatToID;
+    var element2='<div class="box box-danger direct-chat direct-chat-danger" >\
+        <div class="box-header with-border">\
+            <h3 class="box-title">'+userName+'</h3>\
+            <div class="box-tools pull-right">\
+                <button class="btn btn-box-tool"><a href="javascript:close_popup(\''+ chatIdAndUserInSessionId +'\');">&#10005;</a></button>\
+            </div>\
+        </div>\
+        <div class="box-body">\
+            <div class="direct-chat-messages" id="socket-messages'+chatID+'" name="chat-messages">\
+            </div>\
+        </div>\
+        <div class="box-footer">\
+            <div class="input-group">\
+                <input type="text" name="message" placeholder="Escribir Mensaje ..." class="form-control" id="socket-input'+chatID+'">\
+                <span class="input-group-btn">\
+                    <button type="button" href="#" class="btn btn-danger btn-flat" onclick="prueba('+chatID+')">Enviar</button>\
+                </span>\
+            </div>\
+        </div>\
+    </div>';
+    var element = '<div class="popup-box chat-popup" id="'+ chatID +'">';
+    element+=element2;
+    document.getElementById("idunico").innerHTML = document.getElementById("idunico").innerHTML + element;
+
+    popups.unshift(chatID);
+    calculate_popups();
+}
 
 //displays the popups. Displays based on the maximum number of popups that can be displayed on the current viewport width
 function display_popups()
 {
     var right = 220;
-
     var iii = 0;
     for(iii; iii < total_popups; iii++)
     {
@@ -52,9 +179,29 @@ function display_popups()
         element.style.display = "none";
     }
 }
-//creates markup for a new popup. Adds the id to popups array.
-function register_popup(id,name,chatID)
+//creates markup for a new popup. Adds the id to popups array. Also saved an object for session storage
+function register_popup(id,name,chatID,userInSessionID)
 {
+    //Code of Session Storage
+    var chatToSave;
+    //If there are no chats save.
+    if(sessionStorage.getItem(userInSessionID) == null){
+        chatToSave = [{
+            savedName: name,
+            savedChatToId: id
+        }];
+    }
+    else{
+        chatToSave = JSON.parse(sessionStorage.getItem(userInSessionID));
+        chatToSave.push({
+            savedName: name,
+            savedChatToId: id
+        });
+    }
+    var chatsToSaveJson = JSON.stringify(chatToSave);
+    sessionStorage.setItem(userInSessionID, chatsToSaveJson);
+    //End of Session Storage code.
+
     for(var iii = 0; iii < popups.length; iii++)
     {
         //already registered. Bring it to front.
@@ -70,11 +217,12 @@ function register_popup(id,name,chatID)
             return;
         }
     }
+    var chatIdAndUserInSessionId = chatID + "-" + userInSessionID + "-" + id;
     var element2='<div class="box box-danger direct-chat direct-chat-danger" >\
         <div class="box-header with-border">\
             <h3 class="box-title">'+name+'</h3>\
             <div class="box-tools pull-right">\
-                <button class="btn btn-box-tool"><a href="javascript:close_popup(\''+ chatID +'\');">&#10005;</a></button>\
+                <button class="btn btn-box-tool"><a href="javascript:close_popup(\''+ chatIdAndUserInSessionId +'\');">&#10005;</a></button>\
             </div>\
         </div>\
         <div class="box-body">\
@@ -152,11 +300,11 @@ angular.module('chat', [])
         $http.get('../common/userInSession')
             .success(function (data, status, headers, config) {
                 userInSession=data;
+                display_open_chats(userInSession.id);
             }).
             error(function (data, status, headers, config) {
                 // log error
             });
-
         // var socket = new WebSocket("ws://localhost:9000/common/getSocket");
 
         function writeMessages(event){
@@ -285,7 +433,7 @@ angular.module('chat', [])
                 .success(function (data, status, headers, config) {
                     //data.chat  Chat
                     var chat = data.chat;
-                    register_popup(chatToId, name,chat.id);
+                    register_popup(chatToId, name,chat.id, userInSession.id);
                     angular.element('#socket-messages'+chat.id).empty();
 
                     for (var i =0; i < chat.messages.length; i++){
